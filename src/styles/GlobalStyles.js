bac38fb711b3bf961c33934cda9594f37e1bb7cd
vanagila rodrigues <vanagilakedna@gmail.com>
2022-07-22 14:18:48 -0300

    feat: global stylization

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,87 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    outline: 0;
+    list-style: none;
+  }
+
+  :root {
+    /*background pallet*/
+    --background-1: #353543;
+    --background-2: #272833
+
+    /*purple pallet*/
+    --purple-1: #605B91;
+    --purple-2: #3B3754;
+
+    /*font and icons pallet*/
+    --white-font: #FFFFFF;
+    --yellow-dots: #FFC400;
+    --green: #71F26D;
+
+    /*fonts*/
+    --introduction-font: 'Share Tech Mono', monospace;
+    --name-font: 'Montserrat', sans-serif;
+    --text-font: 'Inconsolata', monospace;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  /* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`;
