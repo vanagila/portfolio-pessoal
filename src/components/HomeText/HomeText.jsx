126468da5975ed1d6cc9c24e6582600caaaf93fe
vanagila rodrigues <vanagilakedna@gmail.com>
2022-07-22 20:04:59 -0300

    feat: introduction text

---
+++ src/components/HomeText/HomeText.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export const HomeText = () => {
+  return (
+    <Container>
+      <p>Olá, me chamo</p>
+      <h2>Vanágila</h2>
+      <p>sou desenvolvedora front-end</p>
+    </Container>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import { Nav } from "../../components/Nav/Nav";
 import { FirstSection } from "./styles";
+import { Nav } from "../../components/Nav/Nav";
+import { HomeText } from "../../components/HomeText/HomeText";
 
 export const Home = () => {
   return (
     <FirstSection>
       <Nav />
+      <HomeText />
     </FirstSection>
   );
 };
