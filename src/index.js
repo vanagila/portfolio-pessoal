f16bbb24711a4aeb404c3c9997a41022ca9fae6e
vanagila rodrigues <vanagilakedna@gmail.com>
2022-07-23 22:57:23 -0300

    feat: techs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { TechsProvider } from "./providers/Techs";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TechsProvider>
+      <App />
+    </TechsProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
