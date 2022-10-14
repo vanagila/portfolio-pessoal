1e2848c8ea8f6aa45ede5b7c1465964b4d120a4f
vanagila rodrigues <vanagilakedna@gmail.com>
2022-10-14 20:41:59 -0300

    feat: add illustration

--- src/components/HomeText/index.jsx
+++ src/components/HomeText/index.jsx
@@ -1,10 +1,14 @@
-import { Container, ImageContainer, Text } from "./styles";
+import { Container, Text, GreetingIllustration } from "./styles";
+import me from "../../assets/img/me.gif";
 
 export const HomeText = () => {
   return (
     <Text>
       <Container>
-        <p>Olá, me chamo</p>
+        <GreetingIllustration>
+          <p>Olá, me chamo</p>
+          <img src={me} alt="me" />
+        </GreetingIllustration>
         <h2>Vanágila</h2>
         <p>
           sou desenvolvedora <br />
