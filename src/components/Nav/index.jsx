eb2f84d9c6626afe070058c7d4875818ba58c882
vanagila rodrigues <vanagilakedna@gmail.com>
2022-10-14 18:06:33 -0300

    fix: centralize navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,26 +6,28 @@ import {
   FiGithub,
 } from "react-icons/fi";
 
-import { Container } from "./styles";
+import { Container, NavBar } from "./styles";
 
 export const Nav = () => {
   return (
-    <Container>
-      <button>
-        <FiUser size={30} />
-      </button>
-      <button>
-        <FiFolder size={30} />
-      </button>
-      <button>
-        <FiFileText size={30} />
-      </button>
-      <button>
-        <FiLinkedin size={30} />
-      </button>
-      <button>
-        <FiGithub size={30} />
-      </button>
-    </Container>
+    <NavBar>
+      <Container>
+        <button>
+          <FiUser size={30} />
+        </button>
+        <button>
+          <FiFolder size={30} />
+        </button>
+        <button>
+          <FiFileText size={30} />
+        </button>
+        <button>
+          <FiLinkedin size={30} />
+        </button>
+        <button>
+          <FiGithub size={30} />
+        </button>
+      </Container>
+    </NavBar>
   );
 };
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const NavBar = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export const Container = styled.div`
   width: 300px;
   height: 60px;
@@ -7,9 +12,7 @@ export const Container = styled.div`
 
   display: flex;
   justify-content: space-between;
-  align-items: center;
 
-  margin: 10px 10px 0 10px;
   padding: 0 10px;
 
   background-color: var(--purple-1);
