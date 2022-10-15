b41d6ab7ad7f53b4cb6f8a242808fb55f06c4098
vanagila rodrigues <vanagilakedna@gmail.com>
2022-10-15 09:13:55 -0300

    feat: add new projects

--- src/database/index.jsx
+++ src/database/index.jsx
@@ -1,18 +1,27 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Developer Habits Manager",
-    img: "https://thumbs2.imgbox.com/5d/8b/rfsCKiud_t.png",
-    demo: "https://react-sprint2-kenzie-gestao-de-habitos.vercel.app/",
-    repo: "https://github.com/brunnomiguel/react-sprint2-kenzie-gestao-de-habitos.git",
-    tags: ["React - ", "Styled-Component - ", "Context-API - ", "API"],
+    title: "KenzieHub",
+    img: "https://thumbs2.imgbox.com/43/ad/Lmz1lfGI_t.png",
+    demo: "https://react-entrega-s2-kenzie-hub-vanagila.vercel.app/",
+    repo: "https://github.com/vanagila/kenzie-hub-vanagila",
+    tags: ["React - ", "Styled-Component - ", "API"],
   },
   {
     id: 2,
-    title: "Developer Habits Manager",
-    img: "https://thumbs2.imgbox.com/5d/8b/rfsCKiud_t.png",
-    demo: "https://react-sprint2-kenzie-gestao-de-habitos.vercel.app/",
-    repo: "https://github.com/brunnomiguel/react-sprint2-kenzie-gestao-de-habitos.git",
+    title: "KenzieHead",
+    img: "https://thumbs2.imgbox.com/c2/4b/v2K3pnwy_t.png",
+    demo: "https://react-entrega-s3-kenzishop-com-context-api-vanagila-vanagila.vercel.app/",
+    repo: "https://github.com/vanagila/kenziehead-com-context-api.git",
+    tags: ["React - ", "Styled-Component - ", "Context-API - ", "API"],
+  },
+  {
+    id: 3,
+    title: "BurguerKenzie",
+    img: "https://thumbs2.imgbox.com/e6/d1/EVcylxWR_t.png",
+    demo: "https://hamburgueria-typescript-json-server.vercel.app/dashboard",
+    repo: "https://github.com/vanagila/hamburgueria-typescript-json-server.git",
+    tags: ["TypeScript - ", "Chakra UI - ", "Context-API - ", "API"],
   },
 ];
 
