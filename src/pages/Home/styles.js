184046154cbeb7ac9d97c504122965f029ea81aa
vanagila rodrigues <vanagilakedna@gmail.com>
2022-10-14 22:40:21 -0300

    style: change font unit

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 
 export const FirstSection = styled.section`
-  width: 100%;
   padding: 15px 0;
 `;
 
 export const SecondSection = styled.section`
-  width: 100%;
   padding: 15px 0;
 `;
