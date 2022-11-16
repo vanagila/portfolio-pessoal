d30c279956a6a3260e08dc96a32d9273a94f253e
vanagila rodrigues <vanagilakedna@gmail.com>
2022-11-16 20:22:41 -0300

    feat: button to mail

---
+++ src/components/MailButton/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 135px;
+  height: 30px;
+  border-radius: 5px;
+  font-size: 1.125rem;
+  font-family: var(--introduction-font);
+  color: var(--black-font);
+  background-color: var(--green);
+`;
